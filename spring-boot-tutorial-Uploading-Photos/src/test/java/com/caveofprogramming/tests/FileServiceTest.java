2418a7399453f45ecdafde7f52881d52255ab72b
j4sysiak <j4sysiak@gmail.com>
2018-12-23 07:22:03 +0100

    wqwqwq

--- spring-boot-tutorial-Uploading-Photos/src/test/java/com/caveofprogramming/tests/FileServiceTest.java
+++ spring-boot-tutorial-Uploading-Photos/src/test/java/com/caveofprogramming/tests/FileServiceTest.java
@@ -1,7 +1,9 @@
 package com.caveofprogramming.tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.lang.reflect.Method;
 
@@ -37,6 +39,24 @@ public class FileServiceTest {
 		assertEquals("should be doc", "doc", (String)method.invoke(fileService, "s.doc"));
 		assertEquals("should be jpeg", "jpeg", (String)method.invoke(fileService, "file.jpeg"));
 		assertNull("should be null", (String)method.invoke(fileService, "xyz"));
+	}
+	
+	@Test
+	public void testIsImageExtension() throws Exception {
+		
+		Method method = FileService.class.getDeclaredMethod("isImageExtension", String.class);
+		method.setAccessible(true);
+		
+		assertTrue("png should be valid", (Boolean)method.invoke(fileService, "png"));
+		assertTrue("PNG should be valid", (Boolean)method.invoke(fileService, "PNG"));
+		assertTrue("jpg should be valid", (Boolean)method.invoke(fileService, "jpg"));
+		assertTrue("jpeg should be valid", (Boolean)method.invoke(fileService, "jpeg"));
+		assertTrue("gif should be valid", (Boolean)method.invoke(fileService, "gif"));
+		assertTrue("GIF should be valid", (Boolean)method.invoke(fileService, "GIF"));
+		assertFalse("doc should be invalid", (Boolean)method.invoke(fileService, "doc"));
+		assertFalse("jpg3 should be invalid", (Boolean)method.invoke(fileService, "jpg3"));
+		assertFalse("gi should be invalid", (Boolean)method.invoke(fileService, "gi"));
+		
 	}
 
 }
